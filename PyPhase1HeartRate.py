9f23bfa4592e9b91e7694dd916ea5e668476be4b
mahish-m <59123827+mahish-m@users.noreply.github.com>
2019-12-25 17:18:05 -0500

    Update PyPhase1HeartRate.py

--- PyPhase1HeartRate.py
+++ PyPhase1HeartRate.py
@@ -17,8 +17,48 @@
 #calculate desired 
 #display results
 
-def calculate(age, outputList):
-	if("target" in outputList):
-		ans = int(input("Age: "))
-	if("max" in outputList):
-		ans = int(input("Age: "))
+
+
+#overview 
+#	heart rate calculation calculator
+#	
+#	input: age
+#	output: max beats per minute, target heart rate
+#	calculate: max heart rate/target heart rate
+#
+#	max heart rate (age)
+#		return (220 - age)
+#
+#	*high and low percent are user set 
+#- what percentage of max heart rate do you want to check for - 
+#	default = 70,85*
+#	target heart rate (resting heart rate, max heart rate(age), low percent, high percent)
+#
+#	- check for resting heart rate in the morning
+#
+#	- for target heart rate return both average and range
+#		- Heart rate resevre = (max heart rate - resting)
+#		- low range = (Heart rate reserve * low percent) + resting
+#		- high range = (Heart rate reserve * high percent) + resting
+#		- average = (high range - low range)/2 + low range
+
+def calculate(data):
+	outputStr = ""
+	if("target" in data):
+		
+	elif("max" in options):
+		outputStr = 220 - data['AGE']
+	return outputStr
+def intake():
+	age = int(input("Age: "))
+	options = str(input("Do you want Max Heart Rate(1) or Target Heart Rate(2)? \nType the numbers corresponding to the calculation: "))
+	if('2' in options):
+		default = str(input("Default range or custom range? For default enter Y for custom enter N: "))
+		if("Y" in default or "y" in default):
+			data["Range"] = [70,85]
+		elif("N" in default or "n" in default):
+			low = int(input("Low end range: "))
+			high = int(input("High end range: "))
+			data["Range"] = [low,high]
+	data = {"Age": age, "Tests": options}
+	return data
