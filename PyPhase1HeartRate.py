4cde95152ce5d91d8e4c98469078b6d9c412ea5a
mahish-m <59123827+mahish-m@users.noreply.github.com>
2019-12-25 16:50:22 -0500

    Add files via upload

---
+++ PyPhase1HeartRate.py
@@ -0,0 +1,24 @@
+# #Mahish Mahendarkar
+# #Python Phase 1 Heartrate proj
+# hr max - heartrate maximum
+# 208-.7(h)
+# 65yr old - 163 bpm
+
+# Part 1 -  simple app in ios/android
+# which can make the heartrate calculaiton
+
+# Part 2 -  use the age from a wearable app (eg apple health) collected age 
+
+# Part 3 - create graph shows HR max rate 
+# if you want to hit your hr max use the graph to see if you are hitting HR max - hourly update, 
+
+#Working Logic: collect age input from user via number entry, 
+#collect which output user wants (max heart rate(high end , low end), target heart rate), 
+#calculate desired 
+#display results
+
+def calculate(age, outputList):
+	if("target" in outputList):
+		ans = int(input("Age: "))
+	if("max" in outputList):
+		ans = int(input("Age: "))
